a4e3b265adc843d2998ced45f7a183fa28d18baa
MukeshR-prog <mukeshr1855@gmail.com>
2025-03-15 18:41:29 +0530

    Intial projectsetup with DB connection

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"; 
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ['GET','POST','DELETE','PUT','PATCH'],
+    credentials: true,
+}))
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+mongoose.connect(databaseUrl).then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch(err => {
+    console.error(err.message);
+    process.exit(1);
+});
---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import background from "../../assets/loginBackground.png";
+import victory from "../../assets/victory.svg";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../components/ui/tabs";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const handleLogin = async () => {};
+  const handleSignup = async () => {};
+
+  return (
+    <div className="h-[100vh] w-screen flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
+              <img className="h-[100px]" src={victory} alt="Victory" />
+            </div>
+            <p className="font-medium text-center">
+              Please enter your credentials to proceed.
+            </p>
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <Tabs className="w-3/4" defaultValue="signup">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]: font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                  value="login"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]: font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  SignUp
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className="flex flex-col gap-5 mt-6" value="login">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="rounded-md p-6"
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="rounded-md p-6"
+                />
+                <Button
+                  className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2"
+                  onClick={handleLogin}
+                >
+                  Login
+                </Button>
+              </TabsContent>
+              <TabsContent className="flex flex-col gap-5 mt-4" value="signup">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="rounded-md p-6"
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="rounded-md p-6"
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  className="rounded-md p-6"
+                />
+                <Button
+                  className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2"
+                  onClick={handleSignup}
+                >
+                  SignUp
+                </Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+        <div className="hidden xl:flex justify-center items-center">
+          <img className="h-[500px]" src={background} alt="Background" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
