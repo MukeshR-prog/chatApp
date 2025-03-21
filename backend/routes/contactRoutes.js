f93148da79c63517cdc32fc7b82e095aff0a206b
MukeshR-prog <mukeshr1855@gmail.com>
2025-03-21 23:56:42 +0530

    partial commit in display the direct messages

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/authMiddleware.js";
-import { SearchContact } from "../controllers/contactsController.js";
+import { getContactsForDMList, SearchContact } from "../controllers/contactsController.js";
 const contactRoute = Router();
 
 contactRoute.post('/search',verifyToken,SearchContact)
-
+contactRoute.get('/get-contacts-for-dm',verifyToken,getContactsForDMList)
 
 export default contactRoute;
